1fcfe9f2c4bc28155953a773d6042c229c69bceb
Shao Cheng <astrohavoc@gmail.com>
2017-04-19 19:18:55 +0800

    Do not generate .asm and run assembler, instead generate a null object file. We only need Cmm output anyway.

--- src/Language/PuellaScript/Compiler/Run.hs
+++ src/Language/PuellaScript/Compiler/Run.hs
@@ -7,11 +7,11 @@ module Language.PuellaScript.Compiler.Run where
 import qualified CmmBuildInfoTables as GHC
 import qualified CmmInfo as GHC
 import qualified CmmPipeline as GHC
-import qualified CodeOutput as GHC
 import Control.Monad
 import Control.Monad.IO.Class
 import qualified CorePrep as GHC
 import qualified CoreToStg as GHC
+import qualified DriverPhases as GHC
 import qualified DriverPipeline as GHC
 import qualified DynFlags as GHC
 import qualified GHC
@@ -20,7 +20,6 @@ import qualified Hooks as GHC
 import qualified HscMain as GHC
 import qualified HscTypes as GHC
 import Language.PuellaScript.Compiler.Flags
-import qualified ProfInit as GHC
 import qualified SimplStg as GHC
 import qualified StgCmm
 import qualified Stream
@@ -65,66 +64,51 @@ run flags =
                     next_phase =
                         GHC.hscPostBackendPhase dflags src_flavour hsc_lang
                 output_fn <- GHC.phaseOutputFilename next_phase
-                (outputFilename, _, _) <-
-                    liftIO $ do
-                        let data_tycons = filter GHC.isDataTyCon cg_tycons
-                            location = GHC.ms_location mod_summary
-                        prepd_binds <-
-                            GHC.corePrepPgm
-                                hsc_env
-                                cg_module
-                                location
-                                cg_binds
-                                data_tycons
-                        corePrepHook flags mod_summary prepd_binds
-                        let stg_binds_from_core =
-                                GHC.coreToStg dflags cg_module prepd_binds
-                        stgFromCoreHook flags mod_summary stg_binds_from_core
-                        (stg_binds, cost_centre_info) <-
-                            GHC.stg2stg dflags cg_module stg_binds_from_core
-                        stgHook flags mod_summary stg_binds
-                        us <- GHC.mkSplitUniqSupply 'S'
-                        let cmm_stream =
-                                StgCmm.codeGen
-                                    dflags
-                                    cg_module
-                                    data_tycons
-                                    cost_centre_info
-                                    stg_binds
-                                    cg_hpc_info
-                            cmms =
-                                let run_pipeline us' cmmgroup = do
-                                        let (topSRT', us'') =
-                                                GHC.initUs us' GHC.emptySRT
-                                        (topSRT, cmmgroup') <-
-                                            GHC.cmmPipeline
-                                                hsc_env
-                                                topSRT'
-                                                cmmgroup
-                                        let srt
-                                                | GHC.isEmptySRT topSRT = []
-                                                | otherwise =
-                                                    GHC.srtToData topSRT
-                                        pure (us'', srt ++ cmmgroup')
-                                in void $
-                                   Stream.mapAccumL run_pipeline us cmm_stream
-                        cmmFromStgHook flags mod_summary cmm_stream
-                        cmmHook flags mod_summary cmms
-                        rawcmms <- GHC.cmmToRawCmm dflags cmms
-                        cmmRawHook flags mod_summary rawcmms
-                        (output_filename, (_, stub_c_exists), foreign_fps) <-
-                            GHC.codeOutput
+                liftIO $ do
+                    let data_tycons = filter GHC.isDataTyCon cg_tycons
+                        location = GHC.ms_location mod_summary
+                    prepd_binds <-
+                        GHC.corePrepPgm
+                            hsc_env
+                            cg_module
+                            location
+                            cg_binds
+                            data_tycons
+                    corePrepHook flags mod_summary prepd_binds
+                    let stg_binds_from_core =
+                            GHC.coreToStg dflags cg_module prepd_binds
+                    stgFromCoreHook flags mod_summary stg_binds_from_core
+                    (stg_binds, cost_centre_info) <-
+                        GHC.stg2stg dflags cg_module stg_binds_from_core
+                    stgHook flags mod_summary stg_binds
+                    us <- GHC.mkSplitUniqSupply 'S'
+                    let cmm_stream =
+                            StgCmm.codeGen
                                 dflags
                                 cg_module
-                                output_fn
-                                location
-                                (cg_foreign `GHC.appendStubC`
-                                 GHC.profilingInitCode
-                                     cg_module
-                                     cost_centre_info)
-                                cg_foreign_files
-                                cg_dep_pkgs
-                                rawcmms
-                        pure (output_filename, stub_c_exists, foreign_fps)
-                pure (GHC.RealPhase next_phase, outputFilename)
+                                data_tycons
+                                cost_centre_info
+                                stg_binds
+                                cg_hpc_info
+                        cmms =
+                            let run_pipeline us' cmmgroup = do
+                                    let (topSRT', us'') =
+                                            GHC.initUs us' GHC.emptySRT
+                                    (topSRT, cmmgroup') <-
+                                        GHC.cmmPipeline hsc_env topSRT' cmmgroup
+                                    let srt
+                                            | GHC.isEmptySRT topSRT = []
+                                            | otherwise = GHC.srtToData topSRT
+                                    pure (us'', srt ++ cmmgroup')
+                            in void $
+                               Stream.mapAccumL run_pipeline us cmm_stream
+                    cmmFromStgHook flags mod_summary cmm_stream
+                    cmmHook flags mod_summary cmms
+                    rawcmms <- GHC.cmmToRawCmm dflags cmms
+                    cmmRawHook flags mod_summary rawcmms
+                pure (GHC.RealPhase next_phase, output_fn)
+            GHC.RealPhase (GHC.As _) -> do
+                output_fn <- GHC.phaseOutputFilename GHC.StopLn
+                liftIO $ writeFile output_fn ""
+                pure (GHC.RealPhase GHC.StopLn, output_fn)
             _ -> GHC.runPhase phase_plus input_fn dflags
