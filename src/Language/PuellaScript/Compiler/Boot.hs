2bb1b11894749d6f6266f7634cefe7406e31dd39
Shao Cheng <astrohavoc@gmail.com>
2017-04-24 06:53:19 +0800

    A failed approach, pushing just for reference.

--- src/Language/PuellaScript/Compiler/Boot.hs
+++ src/Language/PuellaScript/Compiler/Boot.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Language.PuellaScript.Compiler.Boot where
 
+import Control.Monad.IO.Class
 import Data.Functor
+import Data.IORef
 import qualified DynFlags as GHC
 import qualified GHC
 import qualified GHC.Paths as GHC
+import qualified HscTypes as GHC
+import qualified Module as GHC
+import qualified Outputable as GHC
 
 boot :: IO ()
 boot =
@@ -20,4 +27,7 @@ boot =
             } `GHC.lang_set`
             Just GHC.Haskell2010 `GHC.gopt_set`
             GHC.Opt_SplitSections
-        pure ()
+        GHC.HscEnv {..} <- GHC.getSession
+        liftIO $ do
+            GHC.EPS {..} <- readIORef hsc_EPS
+            putStrLn $ GHC.showPpr dflags $ GHC.moduleEnvKeys eps_PIT
