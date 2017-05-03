160e47d75a9854e37ae0d3ba8467931ece25edf1
Shao Cheng <astrohavoc@gmail.com>
2017-05-03 09:28:38 +0800

    Deploy a dummy ghc wrapper that pass all args to the real one.

--- app/phc.hs
+++ app/phc.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Language.PuellaScript.Compiler.SelfBuildInfo
+import System.Environment
+import System.Process
+
 main :: IO ()
-main = pure ()
+main = do
+    args <- getArgs
+    callProcess ghc args
--- boot.sh
+++ boot.sh
@@ -2,6 +2,7 @@
 
 export TOP=$PWD/.boot
 export GHC_LIBDIR=`ghc --print-libdir`
+export PHC=`which phc`
 
 rm -rf $TOP
 mkdir -p $TOP/package.conf.d
@@ -13,23 +14,23 @@ ghc-pkg --package-db $TOP/package.conf.d recache
 
 ghc ghc-prim/Setup.hs
 cd ghc-prim
-./Setup configure --builddir $TOP/dist/ghc-prim --package-db=$TOP/package.conf.d --prefix $TOP
-./Setup build --builddir $TOP/dist/ghc-prim
-./Setup install --builddir $TOP/dist/ghc-prim
+./Setup configure --builddir=$TOP/dist/ghc-prim --package-db=$TOP/package.conf.d --prefix $TOP --with-ghc=$PHC
+./Setup build --builddir=$TOP/dist/ghc-prim
+./Setup install --builddir=$TOP/dist/ghc-prim
 sed -i -e 's,^exposed-modules:,exposed-modules: GHC.Prim,' $TOP/package.conf.d/ghc-prim-*.conf
 ghc-pkg --package-db $TOP/package.conf.d recache
 cd ..
 
 ghc integer-gmp/Setup.hs
 cd integer-gmp
-./Setup configure --builddir $TOP/dist/integer-gmp --package-db=$TOP/package.conf.d --prefix $TOP
-./Setup build --builddir $TOP/dist/integer-gmp
-./Setup install --builddir $TOP/dist/integer-gmp
+./Setup configure --builddir=$TOP/dist/integer-gmp --package-db=$TOP/package.conf.d --prefix $TOP --with-ghc=$PHC
+./Setup build --builddir=$TOP/dist/integer-gmp
+./Setup install --builddir=$TOP/dist/integer-gmp
 cd ..
 
 ghc base/Setup.hs
 cd base
-./Setup configure --builddir $TOP/dist/base --package-db=$TOP/package.conf.d --prefix $TOP -finteger-gmp
-./Setup build --builddir $TOP/dist/base
-./Setup install --builddir $TOP/dist/base
+./Setup configure --builddir=$TOP/dist/base --package-db=$TOP/package.conf.d --prefix $TOP --with-ghc=$PHC -finteger-gmp
+./Setup build --builddir=$TOP/dist/base
+./Setup install --builddir=$TOP/dist/base
 cd ..
